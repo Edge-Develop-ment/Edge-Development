d0c4f072963ccd45e4efcdc1d90e99b020cc4f83
Beau Coburn <douglas.coburn@gmail.com>
2023-07-03 14:09:34 -0600

    Add Hero Component

--- edgedevelopmentlandingpage/src/app/page.tsx
+++ edgedevelopmentlandingpage/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Hero from './Hero';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
           <li><a>Contact</a></li>
         </ul>
       </nav>
+      <Hero />
     </main>
   )
 }
