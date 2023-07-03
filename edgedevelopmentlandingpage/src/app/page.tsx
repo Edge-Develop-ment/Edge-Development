1d48c6f80d1215adb9cceb52491ac851b0e5b760
Beau Coburn <douglas.coburn@gmail.com>
2023-07-03 14:02:45 -0600

    Add Logo

--- edgedevelopmentlandingpage/src/app/page.tsx
+++ edgedevelopmentlandingpage/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <nav className='flex flex-row w-full'>
+        <Image src="/edgedevelopmentlandingpage/public/LogoGrayOrange.png" alt="Logo" width={150} height={150}/>
         <ul className='flex flex-row w-full gap-4'>
           <li><a>Something 0</a></li>
           <li><a>Something 1</a></li>
