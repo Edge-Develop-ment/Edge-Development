51c0a66686d574b37e308268ee3acb5b21ed6eea
Beau Coburn <douglas.coburn@gmail.com>
2023-07-04 13:18:00 -0600

    Fixed Logo path and changed GitIgnore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import Hero from './Hero';
 import About from './About';
 import Testimonials from './Testimonials';
 import Footer from './Footer';
+import logo from '../../public/LogoGrayOrange.png';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <nav className='flex flex-row w-full'>
-        <Image src="/edgedevelopmentlandingpage/public/LogoGrayOrange.png" alt="Logo" width={150} height={150}/>
+        <Image src={logo} alt="Logo" width={150} height={150}/>
         <ul className='flex flex-row w-full gap-4'>
           <li><a>Our Work</a></li>
           <li><a>About Us</a></li>
